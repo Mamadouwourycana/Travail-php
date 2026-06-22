15b579822c95d0fbda1a1fd8bcda5089b6789d56
Mamadou Woury Camara <mamadouwourycamara88@mail.com>
2026-06-22 09:53:33 +0000

    modification

---
+++ exo3.php
@@ -0,0 +1,13 @@
+<?php
+
+$user = [
+    "nom" => "Diallo",
+    "prenom" => "Awa",
+    "email" => "awa@gmail.com"
+];
+
+echo "Nom : " . $user["nom"] . "\n";
+echo "Prénom : " . $user["prenom"] . "\n";
+echo "Email : " . $user["email"] . "\n";
+
+?>
