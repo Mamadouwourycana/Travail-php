53727ee0b8b696ed77313467755dc50e34519952
Mamadou Woury Camara <mamadouwourycamara88@mail.com>
2026-06-22 09:44:08 +0000

    modification

---
+++ exo1.php
@@ -0,0 +1,11 @@
+<?php
+
+$produits = ["Ordinateur", "Téléphone", "Imprimante", "Clavier"];
+
+foreach ($produits as $produit) {
+    echo $produit . "\n";
+}
+
+echo "Nombre total de produits : " . count($produits);
+
+?>
